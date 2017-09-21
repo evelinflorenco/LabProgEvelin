a5ada233efc6e12080486736aba333f8a9d4e805
Evelin <Evelin@Evelin-PC>
2017-09-21 13:01:17 -0300

    teste1

---
+++ projeto1/src/projeto1/Projeto1.java
@@ -0,0 +1,41 @@
+package projeto1;
+
+import java.util.Scanner;
+
+public class Projeto1 {
+	
+	 public static void main(String[] args) {
+		 
+	      final int NÚMEROS_A_LER = 3;
+	      Scanner sc = new Scanner(System.in);
+	      int mínimo = Integer.MAX_VALUE;
+	      int máximo = Integer.MIN_VALUE;
+	 
+	      for (int i = 0; i < NÚMEROS_A_LER; i++) {
+	        int num = recebeProximoInteiro(sc);
+	        mínimo = menorNumeroEntre(mínimo, num);
+	        máximo = maiorNumeroEntre(máximo, num);
+	      }
+	 
+	      System.out.println("O menor numero eh: " + mínimo);
+	      System.out.println("O maior numero eh: " + máximo);
+	   }
+	 
+	   private static int maiorNumeroEntre(int numero1, int numero2) {
+	      return numero2 > numero1? numero2: numero1;
+	   }
+	 
+	   private static int menorNumeroEntre(int numero1, int numero2) {
+	      return numero2 < numero1? numero2: numero1;
+	   }
+	 
+	   private static int recebeProximoInteiro(Scanner sc) {
+	      System.out.print("Entre com o proximo inteiro:");
+	      int num = sc.nextInt();
+	      return num;
+	   }
+	 
+	}
+
+ 
+  
